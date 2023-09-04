c369eed31c7ca38b4b921067ee0bde65f118632e
Emad Aldin <eabdulwahid@wallpostdev.com>
2023-09-04 17:58:56 +0400

    refactor some code

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/upload', function() {
-    return view('upload-file');
-});
-
-Route::get('/upload', [SalesController::class, 'create']);
-Route::post('/upload', [SalesController::class, 'upload']);
-Route::get('/batch', [SalesController::class, 'batch']);
-Route::get('/batch/all', [SalesController::class, 'inProgressBatch']);
